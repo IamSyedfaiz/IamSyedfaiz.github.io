5f074460b8d1d2580ce0d52d7b6691daf7cb30f2
Syed faiz~ <faizmiya110@gmail.com>
2022-04-06 16:48:11 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+
+  darkMode:'class',
+  theme: {
+    extend: {
+      animation: {
+        blob: "blob 6s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -60px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
